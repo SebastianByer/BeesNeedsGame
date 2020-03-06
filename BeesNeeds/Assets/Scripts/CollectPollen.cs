622fc52008d7d654da78ce90d42880016f8a5637
Evan <ewhite755@gmail.com>
2020-03-06 18:00:48 -0500

    scoring+enemy wasp

---
+++ BeesNeeds/Assets/Scripts/CollectPollen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectPollen : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        ScoringSystem.theScore += 1;
+        Destroy(gameObject);
+    }
+
+}
---
+++ BeesNeeds/Assets/Scripts/Fly.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fly : MonoBehaviour
+{
+
+    public float playerSpeed;
+    public Vector3 gravity;
+    public float maxSpeed = 5f;
+    Vector3 velocity = Vector3.zero;
+    public float fowardSpeed = 1f;
+
+    bool didPress = false;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            didPress = true;
+        }
+        else
+        {
+            didPress = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        velocity.x = fowardSpeed;
+        transform.position += velocity * Time.deltaTime;
+        if (didPress == true)
+        {
+
+            float amntToMove1 = playerSpeed * Time.deltaTime;
+            transform.Translate(Vector3.up * amntToMove1);
+
+        }
+    }
+}
